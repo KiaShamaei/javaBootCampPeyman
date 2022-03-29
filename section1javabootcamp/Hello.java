2c2665def3e489e96501d027f44a41ebfb31ec50
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-03-29 14:01:49 +0430

    add variable

---
+++ section1javabootcamp/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello kia is here!");
+        System.out.println("java >  python");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java");
+    }
+}
---
+++ section2javabootcamp/Variable.java
@@ -0,0 +1,8 @@
+class Variable  {
+    public static void main(String[] args) {
+        int passenger = 4 ;
+        String bus = "this is first String";
+        System.out.println(bus);
+        System.out.println(passenger);
+    }
+}
