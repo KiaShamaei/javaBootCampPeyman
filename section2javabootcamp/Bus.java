cf5ada178ebb2eed41411af881dc0db8aed3af72
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-03-29 14:23:49 +0430

    this is

---
+++ section2javabootcamp/Bus.java
@@ -0,0 +1,9 @@
+public class Bus {
+    public static void main(String[] args) {
+        int passenger  = 5 ;
+        System.out.println(passenger);
+        String greeting = "This is the Number : ";
+        System.out.println(greeting + " " + passenger );
+    }
+    
+}
